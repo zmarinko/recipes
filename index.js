4262a960471ca3517dc8ad0b0483caeef7eac788
zmarinko1 <mzeljana0205@gmail.com>
2022-09-21 20:46:34 +0200

    m

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { json, static as estatic } from 'express';
 import recipesRouter from './routes/recipes.router.js';
 import usersRouter from './routes/users.router.js';
-
+import path from 'path';
 const app = express();
 app.use(estatic(path.join('client', 'dist', 'client')))
 app.use(json({ limit: '200mb' }));
